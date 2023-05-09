348fabff8dec3a25d907762489e507002f0a245e
harish-hyperDev <coolprogrammer.777@gmail.com>
2023-05-09 19:48:29 +0530

    filtered data to send GET requests based on countdown of  minimum timestamp

--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+import threading
 import time, csv
 import os, sys
 sys.path.insert(0, os.getcwd())     # Adding project base path to sys.path
@@ -36,13 +37,53 @@ def get_time_diff(t1, t2):
 
 def main():
 
-    timestamp_data = get_timestamps()
-    # print("timestamp data ", timestamp_data)
-    print(get_time_diff(timestamp_data[0], timestamp_data[1]))
+    timestamp_data = get_timestamps() #.apply(lambda x: datetime.strftime(x, "%H:%M:%S"))
+    timestamp_datetime_data = [datetime.strptime(x, "%H:%M:%S") for x in timestamp_data]
+    
+    current_time = datetime.now().strftime("%H:%M:%S")
+    current_time = datetime.strptime(current_time, "%H:%M:%S")
+    
+    timestamp_diff = [(x - current_time).total_seconds() if (x - current_time).total_seconds() > 0 else 0 for x in timestamp_datetime_data]
+    
+    filtered_timestamps = []
+    for x in range(0,len(timestamp_diff)):
+        if not timestamp_diff[x] == 0:
+            filtered_timestamps.append(timestamp_datetime_data[x])
+    
+    timestamp_diff = [i for i in timestamp_diff if i != 0]
+    
+
+    print("--------------")
+    print(filtered_timestamps)
+    print(timestamp_diff)
+    print("--------------")
+    
+    min_timestamp_seconds = min(timestamp_diff)
+    timestamp_occurences = filtered_timestamps.count(filtered_timestamps[timestamp_diff.index(min_timestamp_seconds)])
+    print("Min Value countdown - ", min_timestamp_seconds)
+    print("Index of Min Value countdown - ", timestamp_diff.index(min_timestamp_seconds))
+    print("\nMin Value - ", filtered_timestamps[timestamp_diff.index(min_timestamp_seconds)].time())
+    print("Index of Min Value - ", filtered_timestamps.index(filtered_timestamps[timestamp_diff.index(min_timestamp_seconds)]))
+    print("Occurences of Min Value - ", filtered_timestamps.count(filtered_timestamps[timestamp_diff.index(min_timestamp_seconds)]))
+    
+    time.sleep(min_timestamp_seconds)
+    print(timestamp_diff.index(min_timestamp_seconds))
+    
+    
+    while True:
+        count = 0
+        while count < timestamp_occurences:
+            print("sending request")
+            send_request()
+            count += 1
+    
+    # threading.Timer(timestamp_diff.index(min_timestamp_seconds), send_request()).start()
+    # return
     
     # time.sleep(10)
+    '''
     while True:
-        current_time = datetime.now().strftime("%H:%M:%S")
+        # current_time = datetime.now().strftime("%H:%M:%S")
         print(current_time)
         
         timestamp_occurences = 0
@@ -59,6 +100,7 @@ def main():
             # exit(1)
         
         time.sleep(0.5)
+    '''
 
 
 def send_request():
